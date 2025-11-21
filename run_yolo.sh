24c85e1ea6b8b5d1b27ea565ac3267b187a02da6
Axle0405 <donghak0405@naver.com>
2025-11-21 13:55:06 +0900

    Create run_yolo.sh

---
+++ run_yolo.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$1" ]; then
+  echo "Usage: docker run <image> <image_url>"
+  exit 1
+fi
+
+IMAGE_URL="$1"
+
+cd /opt/darknet
+
+echo "==> Downloading image from URL:"
+echo "    $IMAGE_URL"
+
+wget -O input.jpg "$IMAGE_URL"
+
+echo "==> Running YOLOv3 detection..."
+./darknet detector test cfg/coco.data cfg/yolov3.cfg yolov3.weights input.jpg -dont_show -ext_output
